9adbdb423d0453e1f35032cd5ac9a2ccfbe81647
Paolo Diomede <pdiomede@yahoo.com>
2025-10-21 13:33:40 +0200

    Simplify footer to two lines
    - Remove pdiomede credit line
    - Combine version and GitHub link into one line
    - Footer now has only two lines:
      1. GIP-0079 information
      2. Version and GitHub link

--- generate_dashboard.py
+++ generate_dashboard.py
@@ -1933,18 +1933,15 @@ def generate_html_dashboard(indexers: List[Tuple[str, str]], contract_address: s
     # </div>
     # """
     
-    # Add footer with version and credits
+    # Add footer with version and GitHub link
     html_content += f"""    
     <div class="footer">
         <div class="footer-line">
             This dashboard is based on the GIP-0079: Indexer Rewards Eligibility Oracle. More info can be <a href="https://forum.thegraph.com/t/gip-0079-indexer-rewards-eligibility-oracle/6734" target="_blank">found here</a>
         </div>
-        <div class="footer-content">
+        <div class="footer-line">
             <span class="version">v{VERSION}</span>
             <span class="footer-separator">-</span>
-            <span>Created with ❤️ by <a href="https://x.com/pdiomede" target="_blank">pdiomede</a></span>
-        </div>
-        <div class="footer-line">
             <svg class="github-icon" viewBox="0 0 16 16" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>View repo on GitHub <a href="https://github.com/pdiomede/reo-dashboard" target="_blank">here</a>
         </div>
     </div>
